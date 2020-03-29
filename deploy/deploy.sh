8b01a75ee748de12e97240be7a1ebf927d67d933
Francesco Rivola <francesco.rivola@gmail.com>
2020-03-29 20:26:48 +0200

    deleted codeship ci files

--- deploy/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo Deploy image $IMAGE_NAME:$CI_TIMESTAMP
-scp -r ./var/www/deploy/charts/** $USER_NAME@$HOST_NAME:./charts/tribeca-notifier/
-ssh $USER_NAME@$HOST_NAME helm upgrade tribeca-notifier ./charts/tribeca-notifier \
-    --set image=$IMAGE_NAME:$CI_TIMESTAMP \
-    --install --wait
-
